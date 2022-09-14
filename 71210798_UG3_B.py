639122321a84c5ed508078c8b76c8d5771ed2199
iglelasia <93341867+iglelasia@users.noreply.github.com>
2022-09-14 19:15:07 +0700

    Add files via upload

---
+++ 71210798_UG3_B.py
@@ -0,0 +1,18 @@
+panjang = input("Masukan range datanya ya: ")
+panjang = int(panjang)
+kamus = dict()
+for a in range(panjang):
+    if a % 2 ==0:
+        n = a
+        fact = 1
+        for b in range(1, n + 1):
+            fact = fact * b
+        kamus[a] = fact
+
+print(kamus)
+tampil = int(input("Masukkan Data ditampilkan: "))
+
+if tampil in kamus.keys():
+    print(kamus[tampil])
+else:
+    print("Data tidak ditemukan ya")
